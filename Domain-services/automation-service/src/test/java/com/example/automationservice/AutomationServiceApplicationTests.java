96e789ca0ee2011ab90beacbe4d18ac96da33870
sathindu123 <sathindusathsarakumara@gmail.com>
2026-03-30 22:28:12 +0530

    project stucture

---
+++ Domain-services/automation-service/src/test/java/com/example/automationservice/AutomationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.automationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
